cb652c82a9588e2b13c141087389f67421e9fc14
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-25 17:48:28 +0600

    install react router and create page and set route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import NotFound from "./Pages/NotFound/NotFound";
+import Footer from "./Shared/Footer/Footer";
+import Header from "./Shared/Header/Header";
 
 
 
 function App() {
   return (
     <div>
-      <h1>Hello Shop in shop</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Dashboard></Dashboard>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>This is not found or 404 page</h1>
+        </div>
+    );
+};
+
+export default NotFound;
